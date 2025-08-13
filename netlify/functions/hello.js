2bcf09a835da2411fc61b349fa374e4696575bc3
assaadnakhoul <assaad.nakhoul@gmail.com>
2025-08-13 10:28:03 +0200

    Create hello.js

---
+++ netlify/functions/hello.js
@@ -0,0 +1,5 @@
+export const handler = async () => ({
+  statusCode: 200,
+  headers: { 'content-type': 'application/json' },
+  body: JSON.stringify({ ok: true, msg: 'hello from Netlify functions' })
+});
